72a7c47fa31a1d79120f41295de5779f08e01829
Andrew Blackwell <118554835+AndrewBlackwell@users.noreply.github.com>
2023-04-07 23:13:59 -0400

    Add Explanation and Time Complexity.

--- Easy/Valid-Parenthesis.java
+++ Easy/Valid-Parenthesis.java
@@ -31,3 +31,15 @@ public boolean isValid(String s)
 	}
 	return stack.isEmpty();
 }
+
+// We use a stack to keep track of the opening brackets we encounter. 
+// For each character in the string, if it's an opening bracket, we push it onto the stack. 
+// If it's a closing bracket, we check if it matches the top of the stack 
+// (i.e., the most recent opening bracket we encountered). 
+// If it doesn't match, the string is invalid and we return false. If it matches, 
+// we pop the opening bracket from the stack and continue. If the stack is empty at the end of the loop, 
+// it means all opening brackets were matched and the string is valid. 
+// Otherwise, there are unmatched opening brackets and the string is invalid.
+
+// Time-Complexity is O(N) here, where n = the number of items in s.toCharArray().
+
